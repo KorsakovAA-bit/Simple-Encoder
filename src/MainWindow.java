f97f09782bb83b49adce0689c1537988cc346f0a
KorsakovAA <Korsakowaa@mail,ru>
2020-07-30 15:59:02 +0300

    Сompleted the main functions

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,12 +1,9 @@
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 
 public class MainWindow extends JFrame implements Thread.UncaughtExceptionHandler, ActionListener {
     private final int WIN_WIDTH = 500;
@@ -14,6 +11,7 @@ public class MainWindow extends JFrame implements Thread.UncaughtExceptionHandle
 
     private JFileChooser fileChooser = null;
     private final JTextArea textArea = new JTextArea();
+    private final JTextField keyWordField = new JTextField(15);
     private final JButton loadFileButton = new JButton("Load file");
     private final JButton encryptButton = new JButton("Encrypt");
     private final JButton decryptButton = new JButton("Decrypt");
@@ -37,24 +35,31 @@ public class MainWindow extends JFrame implements Thread.UncaughtExceptionHandle
         setSize(WIN_WIDTH, WIN_HEIGHT);
         setTitle("Simple encoder");
         setAlwaysOnTop(false);
+        JScrollPane scrollTextArea = new JScrollPane(textArea);
+
 
         buttonPanel.add(loadFileButton);
+        buttonPanel.add(keyWordField);
         buttonPanel.add(encryptButton);
         buttonPanel.add(decryptButton);
+
 
         loadFileButton.addActionListener(this);
         encryptButton.addActionListener(this);
         decryptButton.addActionListener(this);
 
         add(buttonPanel, BorderLayout.NORTH);
-        add(textArea, BorderLayout.CENTER);
+        add(scrollTextArea, BorderLayout.CENTER);
 
         setVisible(true);
     }
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
-
+        e.printStackTrace();
+        StackTraceElement[] ste = e.getStackTrace();
+        String msg = String.format("Exception in thread %s: \n\t%s: $s\n\t at \n\t%s", t.getName(), e.getClass().getCanonicalName(), e.getMessage(), ste);
+        JOptionPane.showMessageDialog(null, msg, "Exception", JOptionPane.ERROR_MESSAGE);
     }
 
     @Override
@@ -62,15 +67,84 @@ public class MainWindow extends JFrame implements Thread.UncaughtExceptionHandle
         Object src = e.getSource();
         if(src == loadFileButton){
             loadFile();
+        } else if(src == encryptButton){
+            encrypt(src);
+        } else if(src == decryptButton) {
+            decrypt(src);
+        }else{
+            throw new RuntimeException("Unknown source: " + src);
         }
 
     }
 
-    private void loadFile() {
+    private void decrypt(Object src) {
+        char[] keyWordInChar = keyWordField.getText().toCharArray();
+        char[] textForDecryptInChar = textArea.getText().toCharArray();
+        String result = "";
+        int resultValue = 0;
+        for (int i = 0; i < keyWordInChar.length; i++) {
+            resultValue += keyWordInChar[i];
+        }
+        if (resultValue == 0) {
+            JOptionPane.showMessageDialog(null, "Please enter the key", "Exception", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            resultValue = resultValue / keyWordInChar.length * 3;
 
+            for (int i = 0; i < textForDecryptInChar.length; i++) {
+                if (textForDecryptInChar[i] != ' ') {
+                    textForDecryptInChar[i] -= resultValue;
+                }
+                result += textForDecryptInChar[i];
+            }
+            textArea.setText(result);
+        }
+    }
+
+    private void encrypt(Object src) {
+        String textForEncrypt = textArea.getText();
+        String keyWord = keyWordField.getText();
+        char[] keyWordInChar = keyWord.toCharArray();
+        char[] textForEncryptInChar = textForEncrypt.toCharArray();
+        textForEncrypt = "";
+
+        int resultValue = 0;
+        for (int i = 0; i < keyWordInChar.length; i++) {
+            resultValue += keyWordInChar[i];
+        }
+        if(resultValue == 0){
+            JOptionPane.showMessageDialog(null, "Please enter the key", "Exception", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            resultValue = resultValue / keyWordInChar.length * 3;
+
+            for (int i = 0; i < textForEncryptInChar.length; i++) {
+                if (textForEncryptInChar[i] != ' ') {
+                    textForEncryptInChar[i] += resultValue;
+                }
+                textForEncrypt += textForEncryptInChar[i];
+            }
+
+            fileChooser.setDialogTitle("Сохранение файла");
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            int result = fileChooser.showSaveDialog(MainWindow.this);
+            if (result == JFileChooser.APPROVE_OPTION )
+
+                try (OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(fileChooser.getSelectedFile()), StandardCharsets.UTF_8)) {
+                    BufferedWriter writer = new BufferedWriter(osw);
+                    writer.write(textForEncrypt);
+                    JOptionPane.showMessageDialog(MainWindow.this, "Файл '" + fileChooser.getSelectedFile() +
+                            " ) сохранен");
+
+                    writer.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException("File is not found " + src);
+                }
+        }
+    }
+
+    private void loadFile() {
         fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Выбор директории");
-
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         int result = fileChooser.showOpenDialog(MainWindow.this);
         if (result == JFileChooser.APPROVE_OPTION ) {
             textArea.setText(null);
@@ -80,7 +154,6 @@ public class MainWindow extends JFrame implements Thread.UncaughtExceptionHandle
                     String str = reader.readLine();
                     str.replaceAll("[^A-Za-zА-Яа-яё0-9 ]", " ");
                     str.trim();
-                    System.out.println(str);
                     textArea.append(str+System.lineSeparator());
                 }
 
@@ -89,7 +162,6 @@ public class MainWindow extends JFrame implements Thread.UncaughtExceptionHandle
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
         }
     }
 
